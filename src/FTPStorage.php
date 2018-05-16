0d322dee558d7af132e3f4a53ca1c9ad3d662645
patrykb15580 <patrykb15580@gmail.com>
2018-05-16 13:34:03 +0200

    Add download and file_get_contents methods to FTPStorage

--- src/FTPStorage.php
+++ src/FTPStorage.php
@@ -18,13 +18,89 @@ class FTPStorage
 
         ftp_chdir($this->connection, '/');
 
-        if (!$this->dir_exists($directory) && !$this->mkdir($directory)) { return false; }
+        if (!$this->exists($directory) && !$this->mkdir($directory)) { return false; }
 
         $success = ftp_put($this->connection, $destination, $source, FTP_BINARY);
 
         ftp_close($this->connection);
 
         return $success;
+    }
+
+    public function download($source, $destination)
+    {
+        ftp_chdir($this->connection, '/');
+
+        $file = ftp_get($this->connection, $destination , $source, FTP_BINARY);
+
+        ftp_close($this->connection);
+
+        return $file;
+    }
+
+    public function file_get_contents($source)
+    {
+        ftp_chdir($this->connection, '/');
+
+        ob_start();
+        $result = ftp_get($this->connection, "php://output", $source, FTP_BINARY);
+        $data = ob_get_contents();
+        ob_end_clean();
+
+        ftp_close($this->connection);
+
+        return $data;
+    }
+
+    private function exists($path)
+    {
+        $current_dir = ftp_pwd($this->connection);
+        ftp_chdir($this->connection, '/');
+
+        $path = rtrim($path, '/');
+        $path_parts = explode('/', $path);
+
+        if ($this->is_dir($path)) {
+            if (!@ftp_chdir($this->connection, $path)) {
+                return false;
+            }
+        } else {
+            if (!in_array($path, ftp_nlist($this->connection, dirname($path)))) {
+                return false;
+            }
+        }
+
+        ftp_chdir($this->connection, $current_dir);
+
+        return true;
+    }
+
+    public function file_exists($file_path)
+    {
+        if ($this->is_dir($file_path)) {
+            if (!@ftp_chdir($file_path)) {
+                return false;
+            }
+
+            return true;
+        } else {
+            return in_array($file_path, ftp_nlist($this->connection, dirname($file_path)));
+        }
+    }
+
+    private function is_dir($dir)
+    {
+        $current_dir = ftp_pwd($this->connection);
+        ftp_chdir($this->connection, '/');
+
+        if (!@ftp_chdir($this->connection, $dir)) {
+
+            return false;
+        }
+
+        ftp_chdir($this->connection, $current_dir);
+
+        return true;
     }
 
     private function mkdir($directory)
@@ -35,14 +111,17 @@ class FTPStorage
         $directory = ltrim($directory, '/');
         $directory = rtrim($directory, '/');
 
-        foreach (explode('/', $directory) as $dir_name) {
-            if (!$this->dir_exists($dir_name) && !ftp_mkdir($this->connection, $dir_name)) {
+        $path = '';
+        foreach (explode('/', $directory) as $name) {
+            $path .= '/' . $name;
+
+            if (!$this->exists($path) && !ftp_mkdir($this->connection, $name)) {
                 ftp_chdir($this->connection, $current_dir);
 
                 return false;
             }
 
-            ftp_chdir($this->connection, $dir_name);
+            ftp_chdir($this->connection, $name);
         }
 
         ftp_chdir($this->connection, $current_dir);
