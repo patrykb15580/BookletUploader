ca0d882a7d755f3007ed8362b9f22380e5a92135
patrykb15580 <patrykb15580@gmail.com>
2018-05-23 14:47:17 +0200

    Create new connection for every ftp storage operation

--- src/FTPStorage.php
+++ src/FTPStorage.php
@@ -1,88 +1,103 @@
 <?php
 namespace Booklet\Uploader;
 
+use Config;
+
 class FTPStorage
 {
     private $connection;
     private $login;
 
-    function __construct($ip, $login, $password)
+    private function connection()
     {
-        $this->connection = ftp_connect($ip);
-        $this->login = ftp_login($this->connection, $login, $password);
+        $connection = ftp_connect(Config::get('booklet_uploader_ftp_ip'));
+        $login = ftp_login($connection, Config::get('booklet_uploader_ftp_login'), Config::get('booklet_uploader_ftp_password'));
+
+        return ($login) ? $connection : false;
     }
 
-    public function save($source, $destination)
+    public function save($source, $destination, $overwrite = false)
     {
+        $connection = $this->connection();
         $directory = dirname($destination . '/');
 
-        ftp_chdir($this->connection, '/');
+        ftp_chdir($connection, '/');
 
         if (!$this->exists($directory) && !$this->mkdir($directory)) { return false; }
 
-        $success = ($this->exists($destination)) ? false : ftp_put($this->connection, $destination, $source, FTP_BINARY);
+        $success = ($this->exists($destination) && !$overwrite) ? false : ftp_put($connection, $destination, $source, FTP_BINARY);
 
-        ftp_close($this->connection);
+        ftp_close($connection);
 
         return $success;
     }
 
     public function file_get_contents($source)
     {
-        ftp_chdir($this->connection, '/');
+        $connection = $this->connection();
+
+        ftp_chdir($connection, '/');
 
         ob_start();
-        $result = ftp_get($this->connection, "php://output", $source, FTP_BINARY);
+        $result = ftp_get($connection, "php://output", $source, FTP_BINARY);
         $data = ob_get_contents();
         ob_end_clean();
 
-        ftp_close($this->connection);
+        ftp_close($connection);
 
         return $data;
     }
 
-    private function exists($path)
+    public function exists($path)
     {
-        $current_dir = ftp_pwd($this->connection);
-        ftp_chdir($this->connection, '/');
+        $connection = $this->connection();
+
+        $current_dir = ftp_pwd($connection);
+        ftp_chdir($connection, '/');
 
         $path = rtrim($path, '/');
         $path_parts = explode('/', $path);
 
         if ($this->is_dir($path)) {
-            if (!@ftp_chdir($this->connection, $path)) {
+            if (!@ftp_chdir($connection, $path)) {
                 return false;
             }
         } else {
-            if (!in_array($path, ftp_nlist($this->connection, dirname($path)))) {
+            if (!in_array($path, ftp_nlist($connection, dirname($path)))) {
                 return false;
             }
         }
 
-        ftp_chdir($this->connection, $current_dir);
+        ftp_chdir($connection, $current_dir);
+        ftp_close($connection);
 
         return true;
     }
 
     private function is_dir($path)
     {
-        $current_dir = ftp_pwd($this->connection);
-        ftp_chdir($this->connection, '/');
+        $connection = $this->connection();
 
-        if (!@ftp_chdir($this->connection, $path)) {
+        $current_dir = ftp_pwd($connection);
+        ftp_chdir($connection, '/');
+
+        if (!@ftp_chdir($connection, $path)) {
 
             return false;
         }
 
-        ftp_chdir($this->connection, $current_dir);
+        ftp_chdir($connection, $current_dir);
+        ftp_close($connection);
 
         return true;
     }
 
     private function mkdir($directory)
     {
-        $current_dir = ftp_pwd($this->connection);
-        ftp_chdir($this->connection, '/');
+        $connection = $this->connection();
+
+        $current_dir = ftp_pwd($connection);
+        ftp_chdir($connection, '/');
 
         $directory = ltrim($directory, '/');
         $directory = rtrim($directory, '/');
@@ -91,13 +106,14 @@ class FTPStorage
         foreach (explode('/', $directory) as $name) {
             $path .= '/' . $name;
 
-            if (!$this->exists($path) && !ftp_mkdir($this->connection, $path)) {
+            if (!$this->exists($path) && !ftp_mkdir($connection, $path)) {
 
                 return false;
             }
         }
 
-        ftp_chdir($this->connection, $current_dir);
+        ftp_chdir($connection, $current_dir);
+        ftp_close($connection);
 
         return true;
     }
