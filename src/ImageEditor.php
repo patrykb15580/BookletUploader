f8b9b1ce2e09f7054dc19697073acf71606851cf
patrykb15580 <patrykb15580@gmail.com>
2018-06-06 09:36:03 +0200

    Add ImageEditor class

---
+++ src/ImageEditor.php
@@ -0,0 +1,21 @@
+<?php
+namespace Booklet\Uploader;
+
+use WideImage;
+
+class ImageEditor
+{
+    private $file;
+    private $params;
+
+    function __construct($file_contents, $params)
+    {
+        
+    }
+
+    public function transform()
+    {
+
+    }
+
+}
